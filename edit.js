2de02d1cc0bfaa6d96ba5de7ed0777239526738a
Konrad Paluszek <kezsulap@gmail.com>
2024-01-05 23:51:04 +0100

    Support keyboard shortcuts in textarea in edit.html

--- edit.js
+++ edit.js
@@ -48,4 +48,61 @@ function init() {
 		}
 	})
 }
+function split_area() {
+	let area = document.querySelector('#input')
+	let val = area.value;
+	let start = area.selectionStart, end = area.selectionEnd;
+	// console.log('before shift', JSON.stringify(val.slice(0, start)), JSON.stringify(val.slice(start, end)), JSON.stringify(val.slice(end)))
+	if (start != end) {
+		if (start < val.length && val[start] == '\n') start++;
+		if (end > 0 && val[end - 1] == '\n') end--;
+	}
+	while (start > 0 && val[start - 1] != '\n') start--;
+	while (end < val.length && val[end] != '\n') end++;
+	// console.log('after shift', JSON.stringify(val.slice(0, start)), JSON.stringify(val.slice(start, end)), JSON.stringify(val.slice(end)))
+	return [val.slice(0, start), val.slice(start, end).split('\n'), val.slice(end)];
+}
+function textarea_listener(e) {
+	function update(mapper) {
+		let area = document.querySelector('#input')
+		let [bef, content, aft] = split_area();
+		content = content.map(mapper).join('\n')
+		area.value = bef + content + aft;
+		area.setSelectionRange(bef.length, bef.length + content.length)
+		e.preventDefault();
+		e.stopPropagation()
+	}
+	if (e.key == 'Tab') {
+		if (e.shiftKey)
+			update(x => x.length && x[0] == '\t' ? x.slice(1) : x)
+		else
+			update(x => '\t' + x)
+	}
+	function inc_bid(x, val) {
+		let split = x.split(/^(\t*)/)
+		let pref = split.slice(0, split.length - 1).join('')
+		let rest = split[split.length - 1]
+		let bid = rest.split(' ');
+		console.log(pref, rest, bid)
+		try {
+			let tried = bid[0];
+			let par = tried[0] == '(' && tried[tried.length - 1] == ')';
+			let omit_par = par ? tried.slice(1, -1) : tried;
+			let parsed = parse_call(omit_par);
+			if (parsed > 0 && parsed <= HIGHEST_CONTRACT) {
+				let new_call = parsed + val > 0 && parsed + val <= HIGHEST_CONTRACT ? call_to_str(parsed + val) : '--';
+				if (par) new_call = '(' + new_call + ')'
+				return pref + new_call + (bid.length > 1 ? ' ' : '') + bid.slice(1).join(' ')
+			}
+		}
+		catch (e) { if (!(e instanceof ParsingError)) throw e;}
+		return x;
+	}
+	if (e.key.toLowerCase() == 'b' && e.ctrlKey) {
+		update(x => inc_bid(x, 1))
+	}
+	if (e.key.toLowerCase() == 'y' && e.ctrlKey) {
+		update(x => inc_bid(x, -1))
+	}
+}
 init()
