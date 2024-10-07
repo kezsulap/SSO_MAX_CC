1f6dc0ccff64263d42fcc543801cf37319b8a54a
Konrad Paluszek <kezsulap@gmail.com>
2024-10-07 15:39:39 +0200

    Add copying resources into directory specifically for main branch generate.sh

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import os
 def main():
 	script, *files = sys.argv
 	if not files:
@@ -19,8 +20,15 @@ def main():
 	
 	index = subprocess.run(['git', 'show', 'code:index.html'], capture_output=True, text=True).stdout.strip()
 	with open('output.html', 'w') as output_file:
-		index = index.replace('hardcoded=undefined', 'hardcoded=' + output);
+		index = index.replace('resources', 'resources_main').replace('hardcoded=undefined', 'hardcoded=' + output);
 		print(index, file=output_file)
+	subprocess.run(['rm', '-rf', 'resources_main'])
+	os.mkdir('resources_main')
+	for filename in subprocess.run(['git', 'show', 'code:resources/all_files_list'], capture_output=True, text=True).stdout.split('\n'):
+		if not filename:
+			continue
+		with open('resources_main/' + filename, 'w') as output_file:
+			subprocess.run(['git', 'show', 'code:resources/' + filename], stdout=output_file)
 
 if __name__ == '__main__':
 	main()
