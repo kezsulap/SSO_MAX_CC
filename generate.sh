8890cf0a339917e3cb3f2857ee80677569310cbd
Konrad Paluszek <kezsulap@gmail.com>
2024-10-07 18:04:19 +0200

    Take description.txt file from main branch if no file specified

--- generate.sh
+++ generate.sh
@@ -1,1 +1,5 @@
-python3 <(git show code:generate.py) "$@"
+if [ -z "$@" ]; then
+	python3 <(git show code:generate.py) <(git show main:description.txt)
+else
+	python3 <(git show code:generate.py) "$@"
+fi
