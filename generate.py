2ccf9d6d0d6826613f3600bac23ccefcb80df52a
Konrad Paluszek <kezsulap@gmail.com>
2024-10-10 23:18:08 +0200

    Allow local tests to js using generate.py without commiting

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 import sys
 import subprocess
 import os
+import shutil
+
 def escape(c):
 	c = c.replace('\\', '\\\\');
 	c = c.replace('\r', '\\r');
@@ -9,39 +11,60 @@ def escape(c):
 	c = '\'' + c + '\''
 	return c
 
+def get_file(filename, version):
+	if version is None:
+		with open(filename, 'r') as f:
+			return f.read()
+	else:
+		return subprocess.run(['git', 'show', f'{version}:{filename}'], capture_output=True, text=True).stdout
+
+def copy_file(filename, destination, version):
+	if version is None:
+		shutil.copy(filename, destination)
+	else:
+		with open(destination, 'w') as output_file:
+			subprocess.run(['git', 'show', 'code:' + filename], stdout=output_file)
+			
+
 def main():
 	script, *files = sys.argv
+	from_branch = None
+	flag_c = False
+	if files and files[0] == '-c':
+		from_branch = 'code'
+		files = files[1:]
+		flag_c = True
 	diff = False
 	if files and files[0] == '-d':
 		files = files[1:]
 		diff = True
+	content_branch = None
 	if not files:
 		files = ['description.txt']
+		if not flag_c:
+			content_branch = 'main'
 	content = []
 	for filename in files:
-		if type(filename) == str:
-			with open(filename, 'r') as f:
-				c = f.read();
+		c = get_file(filename, content_branch)
 		content.append(escape(c))
 		if diff:
-			content.append(escape(subprocess.run(['git', 'show', 'HEAD:' + filename], capture_output=True, text=True).stdout))
+			content.append(escape(get_file(filename, 'HEAD')))
 	output = '[' + ','.join(content) + ']'
 	
-	index = subprocess.run(['git', 'show', 'code:index.html'], capture_output=True, text=True).stdout.strip()
+	index = get_file('index.html', from_branch)
 	with open('output.html', 'w') as output_file:
 		index = index.replace('resources', 'resources_main').replace('hardcoded=undefined', 'hardcoded=' + output);
 		print(index, file=output_file)
 	subprocess.run(['rm', '-rf', 'resources_main'])
 	os.mkdir('resources_main')
-	for dirname in subprocess.run(['git', 'show', 'code:resources/all_directories_list'], capture_output=True, text=True).stdout.split('\n'):
+	for dirname in get_file('resources/all_directories_list', from_branch).split('\n'):
 		if not dirname:
 			continue
 		os.mkdir('resources_main/' + dirname)
-	for filename in subprocess.run(['git', 'show', 'code:resources/all_files_list'], capture_output=True, text=True).stdout.split('\n'):
+	for filename in get_file('resources/all_files_list', from_branch).split('\n'):
 		if not filename:
 			continue
-		with open('resources_main/' + filename, 'w') as output_file:
-			subprocess.run(['git', 'show', 'code:resources/' + filename], stdout=output_file)
+		copy_file('resources/' + filename, 'resources_main/' + filename, from_branch)
 
 if __name__ == '__main__':
 	main()
